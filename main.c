10665bb74abb6aa7a47c9c3110fb0944b6ca2f23
shreya2007-shraddha <shreyagadge093@gmail.com>
2025-07-28 14:42:57 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 
 int main()
-{
-    int year;
-    printf("ENTER A YEAR:");
-    scanf("%d",&year);
+{float p,r,t,si;
 
-    if((year %4==0&& year %100!=0)||( year %400==0))
-       printf("%d is a leap year:", year);
-    else
-        printf("&d is not a leap year:", year);
+    printf("enter principal ammount:\n");
+    scanf("%f",&p);
+
+    printf("enter rate of interest:\n");
+    scanf("%f",&r);
+
+    printf("enter period of time (in years):\n");
+    scanf("%f",&t);
+
+    si = (p*r*t) / 100;
+
+    printf("simple intrest = %.2f \n" , si);
 
     return 0;
 }
